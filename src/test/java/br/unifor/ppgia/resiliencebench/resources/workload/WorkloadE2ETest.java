ae90066f22f72c27ad2bd80f34061822511b4359
Carlos Mendes <cmendesce@gmail.com>
2024-01-01 10:41:20 -0300

    Add k8s kind

--- src/test/java/br/unifor/ppgia/resiliencebench/resources/workload/WorkloadE2ETest.java
+++ src/test/java/br/unifor/ppgia/resiliencebench/resources/workload/WorkloadE2ETest.java
@@ -4,8 +4,6 @@ import br.unifor.ppgia.resiliencebench.BenchmarkReconciler;
 import br.unifor.ppgia.resiliencebench.ResilienceServiceReconciler;
 import br.unifor.ppgia.resiliencebench.resources.ConfigMapReference;
 import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.junit.AbstractOperatorExtension;
 import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
 import org.junit.jupiter.api.Assertions;
@@ -16,12 +14,9 @@ import java.util.List;
 
 public class WorkloadE2ETest {
 
-  final static KubernetesClient client = new DefaultKubernetesClient();
-
   @RegisterExtension
-  AbstractOperatorExtension operator =  LocallyRunOperatorExtension.builder()
+  static AbstractOperatorExtension operator =  LocallyRunOperatorExtension.builder()
           .waitForNamespaceDeletion(false)
-          .withKubernetesClient(client)
           .withReconciler(new ResilienceServiceReconciler())
           .withReconciler(new BenchmarkReconciler())
           .build();
@@ -36,10 +31,10 @@ public class WorkloadE2ETest {
     workload.setMetadata(new ObjectMetaBuilder().withName(name).build());
     workload.setSpec(spec);
 
-    var workloadClient = client.resources(Workload.class);
-    workloadClient.inNamespace(operator.getNamespace()).resource(workload).create();
+    var workloadClient = operator.resources(Workload.class);
+    workloadClient.resource(workload).create();
 
-    var actualWorkload = workloadClient.inNamespace(operator.getNamespace()).withName(name).get();
+    var actualWorkload = workloadClient.withName(name).get();
     Assertions.assertNotNull(actualWorkload);
   }
 
