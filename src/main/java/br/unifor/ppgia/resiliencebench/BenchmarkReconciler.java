37120ff97f7f76c30832033b39f2395f5cb331b8
Carlos Mendes <cmendesce@gmail.com>
2024-02-19 16:43:57 -0300

    Fixes inconsistencies

--- src/main/java/br/unifor/ppgia/resiliencebench/BenchmarkReconciler.java
+++ src/main/java/br/unifor/ppgia/resiliencebench/BenchmarkReconciler.java
@@ -14,6 +14,8 @@ import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 @ControllerConfiguration
 public class BenchmarkReconciler implements Reconciler<Benchmark> {
 
@@ -23,25 +25,21 @@ public class BenchmarkReconciler implements Reconciler<Benchmark> {
   public UpdateControl<Benchmark> reconcile(Benchmark benchmark, Context<Benchmark> context) {
     var scenarioRepository = new CustomResourceRepository<>(context.getClient(), Scenario.class);
     var workloadRepository = new CustomResourceRepository<>(context.getClient(), Workload.class);
-    var executionRepository = new CustomResourceRepository<>(context.getClient(), ExecutionQueue.class);
 
     var workload = workloadRepository.get(benchmark.getMetadata().getNamespace(), benchmark.getSpec().getWorkload());
     if (workload.isEmpty()) {
-      logger.warn("Workload not found: {}", benchmark.getSpec().getWorkload());
+      logger.error("Workload not found: {}", benchmark.getSpec().getWorkload());
       return UpdateControl.noUpdate();
     }
 
-    var scenariosList = ScenarioFactory.create(benchmark, workload.get()); // TODO handle empty list
-
-    var executionQueueOpt = executionRepository.get(benchmark.getMetadata().getNamespace(), benchmark.getMetadata().getName());
-    ExecutionQueue executionQueue = null;
-    if (executionQueueOpt.isPresent()) {
-      logger.debug("ExecutionQueue already exists: {}", benchmark.getMetadata().getName());
-      executionQueue = executionQueueOpt.get();
-    } else {
-      executionQueue = ExecutionQueueFactory.create(benchmark, scenariosList);
-      executionRepository.create(executionQueue);
+    var scenariosList = ScenarioFactory.create(benchmark, workload.get());
+    if (scenariosList.isEmpty()) {
+      logger.error("No scenarios found for workload: {}", benchmark.getSpec().getWorkload());
+      return UpdateControl.noUpdate();
     }
+
+    var executionRepository = new CustomResourceRepository<>(context.getClient(), ExecutionQueue.class);
+    var executionQueue = getOrCreateQueue(benchmark, executionRepository, scenariosList);
 
     scenariosList.forEach(scenario -> createOrUpdateScenario(scenario, scenarioRepository));
 
@@ -50,6 +48,18 @@ public class BenchmarkReconciler implements Reconciler<Benchmark> {
 
     logger.info("Benchmark reconciled: {}", benchmark.getMetadata().getName());
     return UpdateControl.noUpdate();
+  }
+
+  private ExecutionQueue getOrCreateQueue(Benchmark benchmark, CustomResourceRepository<ExecutionQueue> executionRepository, List<Scenario> scenariosList) {
+    var queue = executionRepository.get(benchmark.getMetadata().getNamespace(), benchmark.getMetadata().getName());
+    if (queue.isPresent()) {
+      logger.debug("ExecutionQueue already exists: {}", benchmark.getMetadata().getName());
+      return queue.get();
+    } else {
+      var queueCreated = ExecutionQueueFactory.create(benchmark, scenariosList);
+      executionRepository.create(queueCreated);
+      return queueCreated;
+    }
   }
 
   private void createOrUpdateScenario(Scenario scenario, CustomResourceRepository<Scenario> scenarioRepository) {
--- src/main/java/br/unifor/ppgia/resiliencebench/Scheduler.java
+++ src/main/java/br/unifor/ppgia/resiliencebench/Scheduler.java
@@ -3,16 +3,12 @@ package br.unifor.ppgia.resiliencebench;
 import br.unifor.ppgia.resiliencebench.execution.queue.ExecutionQueue;
 import br.unifor.ppgia.resiliencebench.execution.queue.Item;
 import br.unifor.ppgia.resiliencebench.execution.scenario.Scenario;
-import br.unifor.ppgia.resiliencebench.support.Annotations;
 import br.unifor.ppgia.resiliencebench.support.CustomResourceRepository;
 import io.fabric8.kubernetes.api.model.batch.v1.Job;
 import io.fabric8.kubernetes.api.model.batch.v1.JobBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
-import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,15 +17,22 @@ import java.time.ZoneId;
 import java.util.Objects;
 import java.util.UUID;
 
-public class Scheduler {
+import static br.unifor.ppgia.resiliencebench.support.Annotations.OWNED_BY;
+
+public class Scheduler implements Watcher<Job> {
 
   private final static Logger logger = LoggerFactory.getLogger(Scheduler.class);
   private final KubernetesClient client;
 
-  private CustomResourceRepository<ExecutionQueue> queueCustomResourceRepository;
+  private final CustomResourceRepository<ExecutionQueue> queueCustomResourceRepository;
+
+  public Scheduler(KubernetesClient client, CustomResourceRepository<ExecutionQueue> queueCustomResourceRepository) {
+    this.client = client;
+    this.queueCustomResourceRepository = queueCustomResourceRepository;
+  }
 
   public Scheduler(KubernetesClient client) {
-    this.client = client;
+    this(client, new CustomResourceRepository<>(client, ExecutionQueue.class));
   }
 
   private Item getNextItem(ExecutionQueue queue) {
@@ -40,38 +43,21 @@ public class Scheduler {
 
   public void run(ExecutionQueue queue) {
     var nextItem = getNextItem(queue);
+    var namespace = queue.getMetadata().getNamespace();
     if (nextItem != null) {
-      var scenario = new CustomResourceRepository<>(client, Scenario.class).get(queue.getMetadata().getNamespace(), nextItem.getScenario()).get();
-      schedule(scenario);
-    }
-  }
+      var benchmark = queue.getMetadata().getName();
+      var executionQueue = queueCustomResourceRepository.get(namespace, benchmark).get(); // TODO tratar erro
 
-  private void schedule(Scenario scenario) {
-    logger.debug("Start: {}", scenario.getMetadata().getName());
-
-    queueCustomResourceRepository = new CustomResourceRepository<>(client, ExecutionQueue.class);
-    var benchmark = scenario.getMetadata().getAnnotations().get(Annotations.OWNED_BY);
-    var executionQueue = queueCustomResourceRepository.get(scenario.getMetadata().getNamespace(), benchmark).get(); // TODO tratar erro
-
-    // Lógica para criar ou atualizar um Scenario
-    if (deveProcessarScenario(scenario, executionQueue)) {
-      if (!existeJobEmExecucao(scenario.getMetadata().getNamespace())) {
-        criarJobParaScenario(scenario);
-        atualizarStatusScenario(scenario, "processing", executionQueue);
+      if (nextItem.isPending()) {
+        if (!existsJobRunning(namespace)) {
+          createJob(namespace, nextItem);
+          updateStatus(nextItem, namespace, "running", executionQueue);
+        }
       }
     }
-    logger.info("End: {}", scenario.getMetadata().getName());
   }
 
-  private boolean deveProcessarScenario(Scenario scenario, ExecutionQueue executionQueue) {
-    var items = executionQueue.getSpec().getItems();
-    var scenarioName = scenario.getMetadata().getName();
-
-    var foundItem = items.stream().filter(item -> item.getScenario().equals(scenarioName)).findFirst();
-    return foundItem.map(Item::isPending).orElse(false);
-  }
-
-  private boolean existeJobEmExecucao(String namespace) {
+  private boolean existsJobRunning(String namespace) {
     var jobs = client.batch().v1().jobs().inNamespace(namespace).list();
     var deve = jobs.getItems().stream().anyMatch(job ->
             Objects.isNull(job.getStatus().getCompletionTime()) && job.getMetadata().getAnnotations().containsKey("scenario"));
@@ -79,15 +65,14 @@ public class Scheduler {
     return deve;
   }
 
-  private void criarJobParaScenario(Scenario scenario) {
+  private void createJob(String namespace, Item scenario) {
     var jobName = UUID.randomUUID().toString();
-    var namespace = scenario.getMetadata().getNamespace();
     var job = new JobBuilder()
             .withApiVersion("batch/v1")
             .withNewMetadata()
             .withName(jobName)
             .withNamespace(namespace)
-            .addToAnnotations("scenario", scenario.getMetadata().getName())
+            .addToAnnotations("scenario", scenario.getScenario())
             .endMetadata()
             .withNewSpec()
             .withBackoffLimit(4)
@@ -102,40 +87,37 @@ public class Scheduler {
             .endSpec()
             .endTemplate().and().build();
 
-    job = client.batch().v1().jobs().resource(job).create();
-
-    client.batch().v1().jobs().resource(job).watch(new Watcher<>() {
-      @Override
-      public void eventReceived(Action action, Job resource) {
-        if (action.equals(Action.MODIFIED)) {
-          if (Objects.nonNull(resource.getStatus().getCompletionTime())) {
-            logger.info("Job finalizado: {}", resource.getMetadata().getName());
-            var scenarioName = resource.getMetadata().getAnnotations().get("scenario");
-            var scenario = new CustomResourceRepository<>(client, Scenario.class).get(namespace, scenarioName).get();
-            var executionQueue = new CustomResourceRepository<>(client, ExecutionQueue.class).get(namespace, scenario.getMetadata().getAnnotations().get(Annotations.OWNED_BY)).get();
-            atualizarStatusScenario(scenario, "finished", executionQueue);
-            run(executionQueue);
-          }
-        }
-      }
-
-      @Override
-      public void onClose(WatcherException cause) {
-
-      }
-    });
-    logger.debug("Job criada: {}", job.getMetadata().getName());
+    var jobsClient = client.batch().v1().jobs();
+    job = jobsClient.resource(job).create();
+    jobsClient.resource(job).watch(this);
+    logger.debug("Created job: {}", jobName);
   }
 
-  private void atualizarStatusScenario(Scenario scenario, String status, ExecutionQueue executionQueue) {
-    executionQueue.getSpec().getItems().stream()
-            .filter(item -> item.getScenario().equals(scenario.getMetadata().getName()))
-            .findFirst()
-            .ifPresent(item -> {
-              executionQueue.getMetadata().setNamespace(scenario.getMetadata().getNamespace());
-              item.setStatus(status);
-              item.setStartedAt(LocalDateTime.now().atZone(ZoneId.of("UTC")).toString());
-              queueCustomResourceRepository.update(executionQueue);
-            });
+  private void updateStatus(Item queueItem, String namespace, String status, ExecutionQueue executionQueue) {
+    queueItem.setStatus(status);
+    queueItem.setStartedAt(LocalDateTime.now().atZone(ZoneId.of("UTC")).toString());
+    executionQueue.getMetadata().setNamespace(namespace); // TODO verificar pq é necessário passar o namespace
+    queueCustomResourceRepository.update(executionQueue);
+  }
+
+  @Override
+  public void eventReceived(Action action, Job resource) { // TODO precisa melhorar esse método. mto emaranhado!
+    var namespace = resource.getMetadata().getNamespace();
+    if (action.equals(Action.MODIFIED)) {
+      if (Objects.nonNull(resource.getStatus().getCompletionTime())) {
+        logger.debug("Finished job: {}", resource.getMetadata().getName());
+        var scenarioName = resource.getMetadata().getAnnotations().get("scenario");
+        var scenario = new CustomResourceRepository<>(client, Scenario.class).get(namespace, scenarioName).get();
+        var executionQueue = new CustomResourceRepository<>(client, ExecutionQueue.class).get(namespace, scenario.getMetadata().getAnnotations().get(OWNED_BY)).get();
+        var queueItem = executionQueue.getSpec().getItems().stream().filter(item -> item.getScenario().equals(scenarioName)).findFirst().get();
+        updateStatus(queueItem, namespace, "finished", executionQueue);
+        run(executionQueue);
+      }
+    }
+  }
+
+  @Override
+  public void onClose(WatcherException cause) {
+    // TODO o que acontece pra cair aqui?
   }
 }
