ac9913f749b351ba33708f6fc606f8808d85707d
Carlos Mendes <cmendesce@gmail.com>
2024-02-19 16:14:38 -0300

    Remove dead code

--- src/main/java/br/unifor/ppgia/resiliencebench/ResilienceBenchOperator.java
+++ src/main/java/br/unifor/ppgia/resiliencebench/ResilienceBenchOperator.java
@@ -5,7 +5,6 @@ public class ResilienceBenchOperator {
     public static void main(String[] args) {
         var operator = new io.javaoperatorsdk.operator.Operator();
         operator.register(new BenchmarkReconciler());
-//        operator.register(new ScenarioReconciler());
         operator.start();
     }
 }
