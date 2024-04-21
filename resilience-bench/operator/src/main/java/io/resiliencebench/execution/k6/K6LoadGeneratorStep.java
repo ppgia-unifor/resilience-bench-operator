c61f0a2333ecc1cd61344c7228bcfc91fe3155fe
Carlos Mendes <cmendesce@gmail.com>
2024-04-21 11:26:38 -0300

    enabling k6 cloud results

--- resilience-bench/operator/src/main/java/io/resiliencebench/execution/k6/K6LoadGeneratorStep.java
+++ resilience-bench/operator/src/main/java/io/resiliencebench/execution/k6/K6LoadGeneratorStep.java
@@ -47,7 +47,9 @@ public class K6LoadGeneratorStep extends ExecutorStep<Job> {
   public List<String> createCommand(Scenario scenario, ScenarioWorkload scenarioWorkload, Workload workload) {
     return Arrays.asList(
             "k6", "run", "/scripts/k6.js",
-            "--out", String.format("json=/results/%s.json", scenario.getMetadata().getName()),
+//            "--out", String.format("json=/results/%s.json", scenario.getMetadata().getName()),
+            "--out", "cloud",
+            "--token", System.getenv("K6_CLOUD_TOKEN"),
             "--vus", String.valueOf(scenarioWorkload.getUsers()),
             "--tag", "workloadName=" + workload.getMetadata().getName(),
             "--duration", workload.getSpec().getDuration() + "s"
