bec9728d44851975e2aadf7c4646ef89f6954dba
Carlos Mendes <cmendesce@gmail.com>
2024-05-24 07:43:23 -0300

    Improving logging

--- resilience-bench/operator/src/main/java/io/resiliencebench/execution/aws/AwsConfig.java
+++ resilience-bench/operator/src/main/java/io/resiliencebench/execution/aws/AwsConfig.java
@@ -40,7 +40,7 @@ public class AwsConfig {
       logger.info("Successfully connected to S3 bucket " + bucketName);
       return new S3FileManager(amazonS3, bucketName);
     } catch (SdkClientException ex) {
-      logger.error("Failed to connect to S3 bucket " + bucketName + ex.getMessage() + ". Using local file manager instead");
+      logger.warn("Failed to connect to S3 bucket " + bucketName + ex.getMessage() + ". Using local file manager instead", ex);
       return new LocalFileManager();
     }
   }
