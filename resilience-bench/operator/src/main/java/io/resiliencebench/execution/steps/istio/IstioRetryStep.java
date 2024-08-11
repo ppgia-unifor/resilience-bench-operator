78147e90290b11518177acb0a1a72fefc700cd72
Carlos Mendes <cmendesce@gmail.com>
2024-08-11 20:37:26 -0300

    adding istio timeout

--- resilience-bench/operator/src/main/java/io/resiliencebench/execution/steps/istio/IstioRetryStep.java
+++ resilience-bench/operator/src/main/java/io/resiliencebench/execution/steps/istio/IstioRetryStep.java
@@ -9,22 +9,25 @@ import io.resiliencebench.resources.scenario.Scenario;
 import io.resiliencebench.resources.service.ResilientService;
 import io.resiliencebench.support.CustomResourceRepository;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.Optional;
 
+import static java.util.Optional.*;
+
 @Service
 public class IstioRetryStep extends IstioExecutorStep<Scenario> {
 
-  private final static Logger log = org.slf4j.LoggerFactory.getLogger(IstioRetryStep.class);
+  private final static Logger log = LoggerFactory.getLogger(IstioRetryStep.class);
 
   public IstioRetryStep(KubernetesClient kubernetesClient, IstioClient istioClient, CustomResourceRepository<ResilientService> serviceRepository) {
     super(kubernetesClient, istioClient, serviceRepository);
   }
 
   @Override
-  protected boolean isApplicable(Scenario scenario) {
+  public boolean isApplicable(Scenario scenario) {
     return scenario
             .getSpec()
             .getConnectors()
@@ -59,19 +62,17 @@ public class IstioRetryStep extends IstioExecutorStep<Scenario> {
     var httpRetry = new HTTPRetry();
     var attempts = (Integer) patternConfig.get("attempts");
     var perTryTimeout = (Integer) patternConfig.get("perTryTimeout");
-
     if (attempts != null && attempts >= 0) {
       httpRetry.setAttempts(attempts);
-
       if (perTryTimeout != null && perTryTimeout > 0) {
         httpRetry.setPerTryTimeout(perTryTimeout + "ms");
       } else {
         log.warn("perTryTimeout must be greater than or equal to 0.");
       }
-      return Optional.of(httpRetry);
+      return of(httpRetry);
     } else {
       log.error("Retry not configured. Attempts and perTryTimeout are required for retry pattern configuration.");
-      return Optional.empty();
+      return empty();
     }
   }
 }
