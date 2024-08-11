47eb19cdcd21eee2fbc89e7fb67db594778a7889
Carlos Mendes <cmendesce@gmail.com>
2024-08-11 11:37:44 -0300

    forcing action to run

---
+++ resilience-bench/operator/src/test/java/io/resiliencebench/execution/steps/EnvironmentStepTest.java
@@ -0,0 +1,13 @@
+package io.resiliencebench.execution.steps;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnvironmentStepTest {
+
+    @Test
+    void waitUntilConditionTest() {
+
+    }
+}
