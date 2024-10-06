e3ef1a90f2b87dbe7194b93084a3f29f3ae0077b
Carlos Mendes <cmendesce@gmail.com>
2024-10-06 15:04:31 -0300

    add missing bean

--- resilience-bench/operator/src/main/java/io/resiliencebench/config/AwsConfig.java
+++ resilience-bench/operator/src/main/java/io/resiliencebench/config/AwsConfig.java
@@ -5,15 +5,18 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsConfig {
 
+  @Bean
   AmazonS3 createS3Client(@Value("${AWS_REGION:us-east-1}") String region, AWSCredentialsProvider credentialsProvider) {
     return AmazonS3ClientBuilder.standard().withCredentials(credentialsProvider).withRegion(region).build();
   }
 
+  @Bean
   AWSCredentialsProvider credentialsProvider() {
     return new DefaultAWSCredentialsProviderChain();
   }
