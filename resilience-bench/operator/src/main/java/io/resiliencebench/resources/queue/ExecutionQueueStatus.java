eb7bc1eb9c4c78692095c87893a7b4d9ffc1584b
Carlos Mendes <cmendesce@gmail.com>
2025-03-16 11:14:17 -0300

    add PrinterColumn annotation

--- resilience-bench/operator/src/main/java/io/resiliencebench/resources/queue/ExecutionQueueStatus.java
+++ resilience-bench/operator/src/main/java/io/resiliencebench/resources/queue/ExecutionQueueStatus.java
@@ -1,10 +1,15 @@
 package io.resiliencebench.resources.queue;
 
+import io.fabric8.crd.generator.annotation.PrinterColumn;
+
 public class ExecutionQueueStatus {
 
 
-  private long running;
+  @PrinterColumn(name = "Pending")
   private long pending;
+  @PrinterColumn(name = "Running")
+  private long running;
+  @PrinterColumn(name = "Finished")
   private long finished;
 
   public ExecutionQueueStatus() {
